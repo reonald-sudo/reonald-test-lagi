7a634776ffb65f8e450364caeefd3d7f238b540f
Mohamad Reonald <reonaldproject@gmail.com>
2023-06-23 21:49:39 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste minus tempora nobis facilis recusandae earum assumenda velit debitis ipsa accusantium neque, illo saepe libero, ducimus quibusdam id distinctio ea! Atque.</p>
 
     <p>paragrap 2</p>
+
+    <ul>
+        <li>apa aja</li>
+    </ul>
 </body>
 
 </html>
