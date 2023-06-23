9f893aae9519c33c29a2451b75b15c071074be81
Mohamad Reonald <reonaldproject@gmail.com>
2023-06-23 21:42:00 +0800

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar github</title>
+</head>
+
+<body>
+    <h1>HALO</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste minus tempora nobis facilis recusandae earum assumenda velit debitis ipsa accusantium neque, illo saepe libero, ducimus quibusdam id distinctio ea! Atque.</p>
+</body>
+
+</html>
